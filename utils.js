2a7336de126e0c2569794b1bebb8917ee6a2ebe2
ali sina sharifi <146426376+alisina80@users.noreply.github.com>
2023-09-29 16:57:01 +0330

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,33 @@
+const showSwal = (title, icon, buttons, callback) => {
+  swal({
+    title,
+    icon,
+    buttons,
+  }).then((result) => callback(result));
+};
+
+const saveIntoLocalStorage = (key, value) => {
+  return localStorage.setItem(key, JSON.stringify(value));
+};
+
+const getFromLocalStorage = (key) => {
+  return JSON.stringify(localStorage.getItem(key));
+};
+
+const getToken = () => {
+  const userInfos = JSON.parse(localStorage.getItem("user"));
+  return userInfos ? userInfos.token : null;
+};
+
+const isLogin = () => {
+  const userInfos = localStorage.getItem("user");
+  return userInfos ? true : false;
+};
+
+export {
+  showSwal,
+  saveIntoLocalStorage,
+  getFromLocalStorage,
+  getToken,
+  isLogin,
+};
